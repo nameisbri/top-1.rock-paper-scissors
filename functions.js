3b9564c21d36a4517b77b952c6afcf3ae8375802
Gabriela <gcdbarreira@gmail.com>
2024-02-27 17:01:24 -0500

    RPS 2.0

--- functions.js
+++ functions.js
@@ -17,11 +17,9 @@ computerScore = 0;
 
 function playRound() {
 
-
-    // 2 player choices (computer & player) 
-
     let computerSelection;  
 
+    const results = document.querySelector('#results');
 
     function getComputerChoice() {
         var random = Math.floor(Math.random() * 3) + 1; 
@@ -33,59 +31,74 @@ function playRound() {
             return "scissors";
         }
     }
-    
 
-
-   // playerSelection = prompt("What's your choice?"); // replace with clicked button ID 
-        // search the DOM for a button click
-        // return the ID of the button clicked
 
     computerSelection = getComputerChoice(); 
 
-    console.log('computer:' + computerSelection);
-    console.log('player:' + playerSelection)
+    playerSelection.toLowerCase;
+
+    if(playerScore < 5 || computerScore < 5 ) {
 
     function playerPoint() {
         playerScore++;
 
     
-        console.log('You win 1 point! ' + playerSelection + 
+        console.log() ;
+
+        results.innerHTML = 'Computer selected: ' + computerSelection + ' / Player selected: ' + playerSelection
+        + '</br> You win this round! ' + playerSelection + 
         ' beats ' + computerSelection + 
-        '. Score: Player ' +  playerScore + ' / Computer ' + computerScore) ;
+        '. Score: Player ' +  playerScore + ' / Computer ' + computerScore;
+
+        if (playerScore == 5 || computerScore == 5 ) {
+            results.innerHTML = 'You win! Final Score: Player ' +  playerScore + ' / Computer ' + computerScore;
+
+            playerScore = 0;
+            computerScore = 0;
+        }
     }
     
     function computerPoint() {
         computerScore++;
     
-        console.log('You lose! ' + computerSelection + 
+        console.log();
+
+        results.innerHTML = 'Computer selected: ' + computerSelection + ' / Player selected: ' + playerSelection
+        + '</br> You lose this round. ' + computerSelection + 
         ' beats ' + playerSelection + 
-        '. Computer wins 1 point. Score: Player ' +  playerScore + ' / Computer ' + computerScore);
+        '. Computer wins 1 point. Score: Player ' +  playerScore + ' / Computer ' + computerScore;
+
+        if (playerScore == 5 || computerScore == 5) {
+            results.innerHTML = 'You lose! Final Score: Player ' +  playerScore + ' / Computer ' + computerScore;
+
+            playerScore = 0;
+            computerScore = 0;
+    
+        } 
     } 
 
-    playerSelection.toLowerCase;
-
     if (playerSelection == computerSelection) {
-        console.log('Its a tie, play again.');
+        results.innerHTML = 'Its a tie, play again. </br>Score: Player ' +  playerScore + ' / Computer ' + computerScore;
         return "It's a tie, play again."  
     } else if (playerSelection == "rock" && computerSelection == "scissors"){
         return playerPoint(playerScore, computerScore);
-      //  return 'You Win! Rock beats Scissors';
     } else if (playerSelection == "rock" && computerSelection == "paper") {
         return computerPoint(playerScore, computerScore);
-     //   return 'You Lose! Paper beats Rock';
     } else if (playerSelection == "paper" && computerSelection == "scissors") {
         return computerPoint(playerScore, computerScore);
-     //   return 'You Lose! Scissors beats Paper';
     } else if (playerSelection == "paper" && computerSelection == "rock") {
         return playerPoint(playerScore, computerScore);
-     //   return 'You Win! Paper beats Rock';
     } else if (playerSelection == "scissors" && computerSelection == "rock") {
         return computerPoint(playerScore, computerScore);
-    //    return 'You Lose! Rocks beats Scissors';
     } else if (playerSelection == "scissors" && computerSelection == "paper") {
         return playerPoint(playerScore, computerScore);
-    //    return 'You Win! Scissors beats Paper';
     }
+
+    } else {
+        results.innerHTML = 'Final Score: Player ' +  playerScore + ' / Computer ' + computerScore;
+    }
+
+
 
 } 
 
